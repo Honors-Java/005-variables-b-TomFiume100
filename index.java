4357fc39eb117e75219644f96fc29edfecf9ac09
TomFiume100 <tomdiamonds100@gmail.com>
2020-10-10 16:31:35 +0000

    I did a thing.

--- index.java
+++ index.java
@@ -1,3 +1,10 @@
+int rectX = 0;
+int rectSize = 500;
+int circleX = rectSize/2;
+int circleSize = 200;
+int color = 200;
+int color2 = 50;
+
 void setup() {
 	size(500, 500);
 }
@@ -15,9 +22,14 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
+  circleX = circleX + 1;
   background(255);
   stroke(0);
+  fill(color,0,0);
+  rect(rectX,rectX,rectSize,rectSize);
+  fill(color2,0,0)
+  ellipse(circleX,circleX,circleSize,circleSize);
+  
 
 
   
